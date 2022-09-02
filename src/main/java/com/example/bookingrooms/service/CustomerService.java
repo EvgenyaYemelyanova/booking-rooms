4500299044a31aa8bbaae355644082ead00f517e
EvgenyaYemelyanova <Evgenya.Yemelyanova@proton.me>
2022-09-02 12:20:58 +0300

    add CustomerService and CustomerServiceImpl

---
+++ src/main/java/com/example/bookingrooms/service/CustomerService.java
@@ -0,0 +1,14 @@
+package com.example.bookingrooms.service;
+
+import com.example.bookingrooms.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer createCustomer(Customer customer);
+    Customer getCustomerById(long id);
+    Customer updateCustomer(long id, Customer customer);
+    void deleteCustomer(long id);
+    List<Customer> getAllCustomers();
+}
