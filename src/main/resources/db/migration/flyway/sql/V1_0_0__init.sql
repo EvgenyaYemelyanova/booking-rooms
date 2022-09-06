edbafabad9438481f116ed1c7ee2e06ac68b8b56
EvgenyaYemelyanova <Evgenya.Yemelyanova@proton.me>
2022-09-06 14:45:55 +0300

    add field Role in class Customer, add field role in init.sql

--- src/main/resources/db/migration/flyway/sql/V1_0_0__init.sql
+++ src/main/resources/db/migration/flyway/sql/V1_0_0__init.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS customers (
     last_name varchar(255),
     email varchar(255) UNIQUE,
     phone_number varchar(255) UNIQUE,
+    role varchar(255),
     password varchar(255) UNIQUE
-    );
+);
