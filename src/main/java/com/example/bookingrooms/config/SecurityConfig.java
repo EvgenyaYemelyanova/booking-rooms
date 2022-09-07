fb213817aaa101ad0c017eddad4f4679cd615c7f
EvgenyaYemelyanova <Evgenya.Yemelyanova@proton.me>
2022-09-07 15:04:49 +0300

    add @EnableGlobalMethodSecurity and PUBLIC_URL for Swagger

--- src/main/java/com/example/bookingrooms/config/SecurityConfig.java
+++ src/main/java/com/example/bookingrooms/config/SecurityConfig.java
@@ -2,8 +2,8 @@ package com.example.bookingrooms.config;
 
 import com.example.bookingrooms.security.JwtConfigurer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,14 +12,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     private final JwtConfigurer jwtConfigurer;
-
     public SecurityConfig(JwtConfigurer jwtConfigurer) {
         this.jwtConfigurer = jwtConfigurer;
     }
-
+    private static final String[] PUBLIC_URL = {
+            "/",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/v2/api-docs",
+            "/rest-openapi/**",
+            "/auth/login"
+    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -27,21 +33,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/customer").hasAnyAuthority("ADMIN", "USER")
-                //.antMatchers(HttpMethod.POST, "/api/**").hasAuthority("ADMIN")
+                .antMatchers(PUBLIC_URL).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .apply(jwtConfigurer);
     }
-
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
