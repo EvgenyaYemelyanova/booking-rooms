b0911ebfcf5625bfe7d9c34680bcd8befeab561b
EvgenyaYemelyanova <Evgenya.Yemelyanova@proton.me>
2022-09-10 13:35:10 +0300

    update migrations init.sql files

--- src/main/resources/db/migration/flyway/sql/V1_0_1__init.sql
+++ src/main/resources/db/migration/flyway/sql/V1_0_1__init.sql
@@ -1,3 +1,3 @@
-INSERT INTO customers (id, first_name, last_name, phone_number, password, role, email )
+INSERT INTO customer (id, first_name, last_name, phone_number, password, role, email )
 VALUES (1, 'Bredli', 'Kyper', '+375331112233','$2a$12$m9ILD7uuSAav9sp067Yd1e4AneNJoqjzciZT6LVEQGL64iSU5ws1W',
         'ADMIN', 'user@gmail.com');
